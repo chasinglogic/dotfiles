f838206d2fb7aa49dbc115e5938c05fd140a95d2
Mathew Robinson <mathew@chasinglogic.io>
2020-06-01 10:40:29 +0100

    Remove unused functions

--- .functions.sh
+++ .functions.sh
@@ -3,45 +3,15 @@
 #############
 
 function dotfiles() {
-  cd $(dfm where)
-}
-
-function et() {
-    emacsclient -nw $@
-}
-
-function ec() {
-    et $@
+  cd $DOTFILES
 }
 
 function page() {
   $@ | less
 }
 
-function tmux_session_name() {
-  if [ -z $1 ]; then
-    return "default"
-  fi
-
-  echo $1 |\
-  awk -F\/ '{print $(NF)}' |\
-  sed 's/\./-/g'
-}
-
-function tmux_session() {
-  SESS_NAME=$1
-  if [[ -z $(tmux list-sessions | grep "^$SESS_NAME") ]]; then
-    echo "Creating $SESS_NAME"
-    tmux new-session -s $SESS_NAME -d
-  else
-    echo "$SESS_NAME already exists."
-  fi
-
-  if [[ -n $TMUX ]]; then
-    tmux switch-client -t $SESS_NAME
-  else
-    tmux attach-session -t $SESS_NAME
-  fi
+function redact() {
+  $@ 1>/dev/null 2>/dev/null
 }
 
 function sp() {
@@ -54,49 +24,30 @@ function sp() {
     PROJECT=$(projector find $1)
   fi
 
-  SESS_NAME=$(tmux_session_name $PROJECT)
-  CURRENT_SESSION=""
-  if [[ -n $TMUX ]]; then
-    CURRENT_SESSION=$(tmux list-sessions | grep '(attached)' | awk '{ print $1 }' | sed 's/:$//')
-  fi
-
   cd $PROJECT
-  if [[ "$SESS_NAME" != "$CURRENT_SESSION" ]]; then
-    tmux_session $SESS_NAME
-    return
-  fi
-
-  if [[ -d $(pwd)/.git ]]; then
-    NAME=$(basename $(git rev-parse --show-toplevel))
-    if [[ -d ~/.virtualenvs/$NAME ]]; then
-      workon $NAME
-    fi
+  if [[ -d $PROJECT/env ]]; then
+    v
   fi
 }
 
 function v() {
-  if [[ -d .git ]]; then
-    NAME=$(basename $(git rev-parse --show-toplevel 2>/dev/null))
-  else
-    NAME=$(basename $(pwd))
+  TOP_LEVEL=$(git rev-parse --show-toplevel 2>/dev/null)
+  if [[ $? != 0 ]]; then
+    TOP_LEVEL=$(pwd)
   fi
 
-  if [[ -d $HOME/.virtualenvs/$NAME ]]; then
-    workon $NAME
-  else
-    mkvirtualenv $NAME
+  NAME=$(basename $TOP_LEVEL)
+  ENVDIR="$TOP_LEVEL/env"
+  if [[ ! -d $ENVDIR ]]; then
+    python3 -m venv --prompt $NAME $ENVDIR
   fi
-}
 
-function t() {
-  SESS_NAME=$(tmux_session_name $(pwd))
-  tmux_session $SESS_NAME
-}
+  if [[ -n $(env | grep VIRTUAL_ENV) ]]; then
+    deactivate
+  fi
 
-export VIM_PROG=vim
-if [[ -x $(which nvim) ]]; then
-  export VIM_PROG=nvim
-fi
+  source $ENVDIR/bin/activate
+}
 
 function nv() {
   if test $# -gt 0; then
@@ -104,32 +55,6 @@ function nv() {
   elif test -f Session.vim; then
     env $VIM_PROG -S
   else
-    env $VIM_PROG -c Obsession
+    env $VIM_PROG -c Obsession .
   fi
-}
-
-# Poor man's virtualenvwrapper
-
-function checkvenvdir() {
-  if [[ ! -d $HOME/.virtualenvs ]]; then
-    mkdir -p $HOME/.virtualenvs
-  fi
-}
-
-function workon() {
-  if [[ -d $HOME/.virtualenvs/$1 ]]; then
-    source $HOME/.virtualenvs/$1/bin/activate
-  fi
-}
-
-function mkvirtualenv() {
-  checkvenvdir
-  new_venv_path="$HOME/.virtualenvs/$1"
-  if [[ -d $new_venv_path ]]; then
-    echo "Virtualenv $1 already exists!"
-    return 1
-  fi
-
-  python3 -m venv $new_venv_path
-  workon $1
 }
