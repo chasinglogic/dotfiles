d28a74ecbf473e44e5c7dc538862ef5d42c2dd13
Mathew Robinson <mathew@chasinglogic.io>
2023-11-03 22:07:01 +0000

    Switch to literate config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -137,7 +137,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +dragndrop +pandoc +present +hugo)               ; organize your plain life in plain text
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -174,5 +174,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
