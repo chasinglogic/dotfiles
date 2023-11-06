a00bffb72921cb3abc2cd0cbc078dd9eb1eb47fb
Mathew Robinson <mathew@chasinglogic.io>
2023-11-06 19:58:33 +0000

    Add nerdFonts and tmux packages

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -49,33 +49,19 @@ in {
       hugo
       indent
       libtool
-      nodejs
+      nerdFonts
       ninja
+      nodejs
       pandoc
       podman
-      (python3.withPackages (ps: with ps; [ requests ]))
       ripgrep
       rustup
+      tmux
       tree-sitter
       twine
       yamllint
 
-      # # Adds the 'hello' command to your environment. It prints a friendly
-      # # "Hello, world!" when run.
-      # pkgs.hello
-
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
+      (python3.withPackages (ps: with ps; [ requests ]))
     ] ++ osSpecificPackages;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
