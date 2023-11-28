14ee4c7484f2470fd0cae910be8ab644fada7a3a
Mathew Robinson <mathew@chasinglogic.io>
2023-11-28 08:53:39 +0000

    Add that ripgrep 14 goodness

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -79,7 +79,7 @@ require("lazy").setup({
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = 'dracula',
+				theme = 'dracula-nvim',
 				component_separators = '|',
 				section_separators = '',
 			},
