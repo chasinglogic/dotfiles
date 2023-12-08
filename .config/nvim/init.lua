4ad14193f0c191059fa8a574ac9c215b6230fe2b
Mathew Robinson <mathew@chasinglogic.io>
2023-12-08 16:50:11 +0000

    make tab actually do completion how i want

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -291,7 +291,7 @@ cmp.setup({
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
+        cmp.confirm({ select = true })
       elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
