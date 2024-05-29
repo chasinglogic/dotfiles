92dfc9c76e65602560ca786ced92463a5290bfef
Mathew Robinson <mathew@chasinglogic.io>
2024-05-29 06:24:54 +0100

    remove recursive symlink

--- .local/bin/wherefore-art-thou-romeo.sh
+++ .local/bin/wherefore-art-thou-romeo.sh
@@ -1,1 +1,18 @@
-/Users/mathewrobinson/.config/dfm/profiles/chasinglogic/.config/dfm/profiles/chasinglogic/.local/bin/wherefore-art-thou-romeo.sh
+#!/usr/bin/env bash
+# Yes I know it's wherefore means why.
+
+TARGET="$1"
+
+echo "Searching for $1"
+
+for secret in $(aws --profile production secretsmanager list-secrets --filter 'Key=name,Values=core/' | jq -r .SecretList[].ARN); do
+    echo "Checking $secret"
+    FOUND=$(aws secretsmanager get-secret-value --secret-id "$secret" | rg "$TARGET")
+    if [[ -n "$FOUND" ]]; then
+        echo "Found $TARGET in secret $secret."
+        exit 0
+    fi
+done
+
+echo "Failed to find $TARGET."
+exit 10
