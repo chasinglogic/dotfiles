37f4c025480afa26f44cbdb6a3d209ee63f58c68
Mathew Robinson <mathew@chasinglogic.io>
2024-06-25 10:26:30 +0100

    add sed and grep aliases for macos

--- .aliases.sh
+++ .aliases.sh
@@ -10,13 +10,13 @@ alias g="git"
 
 alias venv="python3 -m venv"
 
-if [[ -n "$VIM_PROG" ]]; then	
-    alias vim="\$VIM_PROG"
+if [[ -n "$VIM_PROG" ]]; then
+	alias vim="\$VIM_PROG"
 fi
 
 alias e="\$EDITOR"
 if [[ "$VIM_PROG" == "nvim" ]]; then
-    alias vimdiff="nvim -d"
+	alias vimdiff="nvim -d"
 fi
 
 alias k='kubectl'
@@ -44,3 +44,8 @@ alias aws-login-pip="aws --profile internal-services codeartifact login --tool p
 alias aws-login-twine="aws --profile internal-services codeartifact login --tool twine --repository tillo-python --domain tillo --domain-owner 307488140247 --region eu-west-1"
 alias aws-login-helm="aws --profile internal-services ecr get-login-password | helm registry login --username AWS --password-stdin 307488140247.dkr.ecr.eu-west-1.amazonaws.com"
 alias aws-login-docker="AWS_PROFILE=internal-services aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 307488140247.dkr.ecr.eu-west-1.amazonaws.com"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+	alias sed="gsed"
+	alias grep="ggrep"
+fi
