0ebe685fbdee512ee8cebfcb4e2f08476c650513
Mathew Robinson <mathew@chasinglogic.io>
2024-10-24 08:24:37 +0100

    delete non-comma scripts

--- .local/bin/fix_mac_framework_links.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo ln -s  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreFoundation.framework /Library/Frameworks/CoreFoundation.framework
-sudo ln -s  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/Security.framework /Library/Frameworks/Security.framework
