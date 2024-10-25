ab90e253729641b9bfe7c6c770ace7f2ebb387a3
Mathew Robinson <mathew@chasinglogic.io>
2024-10-25 09:55:51 +0100

    simplify prompt back to basics

--- .prompt.bash
+++ .prompt.bash
@@ -12,14 +12,14 @@ PINK="$(tput setaf 165)"
 CYAN="$(tput setaf 17)"
 WHITE="$(tput setaf 255)"
 GREY="$(tput setaf 240)"
+ORANGE="$(tput setaf 209)"
 RESET="\e[0m"
 
 NO_COLOR="${RESET}"
 COMMAND_STATUS_COLOR="$(tput bold)${RED}"
-LAMBDA_COLOR="${YELLOW}"
-DELTA_COLOR="${YELLOW}"
-INFO_COLOR="$(tput bold)$(tput setaf 9)"
-CONNECTOR_COLOR="${INFO_COLOR}"
+LAMBDA_COLOR="${RESET}${YELLOW}"
+DELTA_COLOR="${RESET}${YELLOW}"
+INFO_COLOR="$(tput bold)${WHITE}"
 
 SEPARATOR=" "
 
@@ -35,8 +35,6 @@ function __kube_context_prompt {
   active_context=$(kubectl config current-context 2>/dev/null)
   if [[ "$active_context" == "production" ]]; then
     echo "${RED}${active_context}"
-  elif [[ "$active_context" == "staging" ]]; then
-    echo "${YELLOW}${active_context}"
   else
     echo "${active_context}"
   fi
@@ -47,8 +45,6 @@ function __git_branch_prompt {
   branch=${ref#refs/heads/}
   if [[ "$branch" == "main" || "$branch" == "master" ]]; then
     echo "${RED}${branch}"
-  elif [[ "$branch" == "develop" ]]; then
-    echo "${YELLOW}${branch}"
   else
     echo "${branch}"
   fi
@@ -58,23 +54,14 @@ function __prompt_command {
   RET="$?"
   PS1=""
 
-  INFO=""
-
   function __info_segment {
-    INFO=$(add_sep_if_required "${INFO}")
-    INFO+="\[${INFO_COLOR}\][${1}: ${2}\[${INFO_COLOR}\]]"
+    PS1=$(add_sep_if_required "${PS1}")
+    PS1+="\[${INFO_COLOR}\](${1}: ${2}\[${INFO_COLOR}\])"
   }
-
-  __info_segment "dir" "\w"
 
   if [[ -n "${VIRTUAL_ENV_PROMPT}" ]]; then
     VENV_NAME=${VIRTUAL_ENV_PROMPT//[() ]/}
     __info_segment "venv" "$VENV_NAME"
-  fi
-
-  branch=$(__git_branch_prompt)
-  if [[ -n "$branch" ]]; then
-    __info_segment "git" "$branch"
   fi
 
   active_context=$(__kube_context_prompt)
@@ -82,16 +69,16 @@ function __prompt_command {
     __info_segment "kube" "$active_context"
   fi
 
-  if [[ -n "${AWS_PROFILE}" ]]; then
-    __info_segment "aws" "$AWS_PROFILE"
-  fi
-
-  if [[ -n "${INFO}" ]]; then
-    PS1+="${CONNECTOR_COLOR}┌─${INFO}\n${CONNECTOR_COLOR}└─ "
-  fi
-
   if [[ "${RET}" != "0" ]]; then
     PS1+="\[${COMMAND_STATUS_COLOR}\]!!\[${NO_COLOR}\] "
+  fi
+
+  PS1=$(add_sep_if_required "$PS1")
+  PS1+="\w "
+
+  branch=$(__git_branch_prompt)
+  if [[ -n "$branch" ]]; then
+    PS1+="$branch "
   fi
 
   if [[ "$(git diff --shortstat 2>/dev/null | tail -n1)" != "" ]]; then
