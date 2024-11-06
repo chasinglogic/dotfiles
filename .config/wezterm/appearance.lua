e09e425afd6844b849b418bd626ab9281a0da536
Mathew Robinson <mathew@chasinglogic.io>
2024-11-06 08:49:29 +0000

    improve status display

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -51,7 +51,6 @@ local function segments_for_right_status(window)
 end
 
 wezterm.on('update-status', function(window, _)
-    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
     local segments = segments_for_right_status(window)
 
     local color_scheme = window:effective_config().resolved_palette
@@ -84,16 +83,19 @@ wezterm.on('update-status', function(window, _)
 
     for i, seg in ipairs(segments) do
         local is_first = i == 1
+        local is_last = i == #segments
 
         if is_first then
             table.insert(elements, { Background = { Color = 'none' } })
         end
-        table.insert(elements, { Foreground = { Color = gradient[i] } })
-        table.insert(elements, { Text = SOLID_LEFT_ARROW })
 
         table.insert(elements, { Foreground = { Color = fg } })
         table.insert(elements, { Background = { Color = gradient[i] } })
         table.insert(elements, { Text = ' ' .. seg .. ' ' })
+
+        if is_last then
+            table.insert(elements, { Text = ' ' })
+        end
     end
 
     window:set_right_status(wezterm.format(elements))
