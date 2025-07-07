950c977a8f9894ca35af9887679b98d1cad2b71c
Mathew Robinson <mrobinson@neurolabs.ai>
2025-07-07 13:35:58 +0100

    remove redundant wezterm configs, add zellij config

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -27,10 +27,9 @@ function module.apply_to_config(config)
         config.window_decorations = "RESIZE"
     end
 
-    -- This hides status info that is useful so always show it.
-    config.hide_tab_bar_if_only_one_tab = false
-    config.tab_bar_at_bottom = false
-    config.use_fancy_tab_bar = false
+    config.hide_tab_bar_if_only_one_tab = true
+    -- config.tab_bar_at_bottom = false
+    -- config.use_fancy_tab_bar = false
 
     config.enable_scroll_bar = false
     config.window_padding = {
@@ -40,90 +39,5 @@ function module.apply_to_config(config)
         top = '0.2cell',
     }
 end
-
-local function segments_for_right_status(window)
-    return {
-        window:active_workspace(),
-        wezterm.strftime('%a %b %-d %H:%M'),
-        wezterm.hostname(),
-    }
-end
-
-wezterm.on('update-status', function(window, _)
-    local segments = segments_for_right_status(window)
-
-    local color_scheme = window:effective_config().resolved_palette
-    -- Note the use of wezterm.color.parse here, this returns
-    -- a Color object, which comes with functionality for lightening
-    -- or darkening the colour (amongst other things).
-    local bg = wezterm.color.parse(color_scheme.background)
-    local fg = color_scheme.foreground
-
-    -- Each powerline segment is going to be coloured progressively
-    -- darker/lighter depending on whether we're on a dark/light colour
-    -- scheme. Let's establish the "from" and "to" bounds of our gradient.
-    local gradient_to, gradient_from = bg
-    gradient_from = gradient_to:lighten(0.2)
-
-    -- Yes, WezTerm supports creating gradients, because why not?! Although
-    -- they'd usually be used for setting high fidelity gradients on your terminal's
-    -- background, we'll use them here to give us a sample of the powerline segment
-    -- colours we need.
-    local gradient = wezterm.color.gradient(
-        {
-            orientation = 'Horizontal',
-            colors = { gradient_from, gradient_to },
-        },
-        #segments -- only gives us as many colours as we have segments.
-    )
-
-    -- We'll build up the elements to send to wezterm.format in this table.
-    local elements = {}
-
-    for i, seg in ipairs(segments) do
-        local is_first = i == 1
-        local is_last = i == #segments
-
-        if is_first then
-            table.insert(elements, { Background = { Color = 'none' } })
-        end
-
-        table.insert(elements, { Foreground = { Color = fg } })
-        table.insert(elements, { Background = { Color = gradient[i] } })
-        table.insert(elements, { Text = ' ' .. seg .. ' ' })
-
-        if is_last then
-            table.insert(elements, { Text = ' ' })
-        end
-    end
-
-    window:set_right_status(wezterm.format(elements))
-end)
-
--- local function tab_title(tab_info)
---     local title = tab_info.tab_title
---     -- if the tab title is explicitly set, take that
---     if title and #title > 0 then
---         return title
---     end
---     -- Otherwise, use the process name of the active pane
---     -- in that tab
---     local procname = tab_info.active_pane.foreground_process_name
---     if procname == "" then
---         return "uknown"
---     end
-
---     return procname
--- end
-
--- Tried playing with this, I have to return everythig not just the title of the
--- tab. so needs more playing then I have time for right now
--- wezterm.on(
---     'format-tab-title',
---     function(tab, tabs, panes, config, hover, max_width)
---         tab.title = tab_title(tab)
---         return tab
---     end
--- )
 
 return module
--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,145 +1,25 @@
 local wezterm = require 'wezterm'
-local projects = require 'projects'
-local sys = require 'sys'
 local actions = wezterm.action
 local module = {}
 
 function module.apply_to_config(config)
     local keys = {
-        -- Tmux emulation bindings
-        {
-            key = 'r',
-            mods = 'LEADER',
-            action = actions.ReloadConfiguration,
-        },
-        {
-            key = 'S',
-            mods = 'LEADER',
-            action = actions.SplitVertical,
-        },
-        {
-            key = 'v',
-            mods = 'LEADER',
-            action = actions.SplitHorizontal,
-        },
-        {
-            key = 'c',
-            mods = 'LEADER',
-            action = actions.SpawnTab('CurrentPaneDomain'),
-        },
-        {
-            key = 'n',
-            mods = 'LEADER',
-            action = actions.ActivateTabRelative(1),
-        },
-        {
-            key = 'p',
-            mods = 'LEADER',
-            action = actions.ActivateTabRelative(-1),
-        },
-        {
-            key = 'h',
-            mods = 'LEADER',
-            action = actions.ActivatePaneDirection('Left'),
-        },
-        {
-            key = 'j',
-            mods = 'LEADER',
-            action = actions.ActivatePaneDirection('Down'),
-        },
-        {
-            key = 'k',
-            mods = 'LEADER',
-            action = actions.ActivatePaneDirection('Up'),
-        },
-        {
-            key = 'l',
-            mods = 'LEADER',
-            action = actions.ActivatePaneDirection('Right'),
-        },
-        {
-            key = 'x',
-            mods = 'LEADER',
-            action = actions.CloseCurrentPane({ confirm = false }),
-        },
-        {
-            key = 'm',
-            mods = 'LEADER',
-            action = wezterm.action_callback(function(win, pane)
-                local tab = win:active_tab()
-                for _, p in ipairs(tab:panes()) do
-                    if p:pane_id() ~= pane:pane_id() then
-                        p:activate()
-                        win:perform_action(actions.CloseCurrentPane { confirm = false }, p)
-                    end
-                end
-            end),
-        },
-        {
-            key = 'z',
-            mods = 'LEADER',
-            action = actions.TogglePaneZoomState,
-        },
-
-        -- Adding in workspace features
-        -- Prompt for a name to use for a new workspace and switch to it.
-        {
-            key = 'w',
-            mods = 'LEADER',
-            action = actions.PromptInputLine {
-                description = wezterm.format {
-                    { Attribute = { Intensity = 'Bold' } },
-                    { Foreground = { AnsiColor = 'Fuchsia' } },
-                    { Text = 'Enter name for new workspace' },
-                },
-                action = wezterm.action_callback(function(window, pane, line)
-                    -- line will be `nil` if they hit escape without entering anything
-                    -- An empty string if they just hit enter
-                    -- Or the actual line of text they wrote
-                    if line then
-                        window:perform_action(
-                            actions.SwitchToWorkspace {
-                                name = line,
-                            },
-                            pane
-                        )
-                    end
-                end),
-            },
-        },
-        {
-            key = 'p',
-            mods = 'LEADER',
-            action = projects.choose_project(),
-        },
-
-        -- Show the launcher in fuzzy selection mode and have it list all workspaces
-        -- and allow activating one.
-        {
-            key = 'o',
-            mods = 'LEADER',
-            action = actions.ShowLauncherArgs {
-                flags = 'FUZZY|WORKSPACES',
-            },
-        },
     }
 
     for i = 1, 9 do
         -- LEADER + number to activate that tab
+        -- table.insert(keys, {
+        --     key = tostring(i),
+        --     mods = 'LEADER',
+        --     action = actions.ActivateTab(i - 1),
+        -- })
+
+        -- alt + number to activate that tab
         table.insert(keys, {
             key = tostring(i),
-            mods = 'LEADER',
+            mods = 'ALT',
             action = actions.ActivateTab(i - 1),
         })
-
-        if sys.is_linux then
-            -- alt + number to activate that tab
-            table.insert(keys, {
-                key = tostring(i),
-                mods = 'ALT',
-                action = actions.ActivateTab(i - 1),
-            })
-        end
     end
 
     -- Rather than emitting fancy composed characters when alt is pressed, treat the
@@ -147,7 +27,7 @@ function module.apply_to_config(config)
     config.send_composed_key_when_left_alt_is_pressed = false
     config.send_composed_key_when_right_alt_is_pressed = false
 
-    config.leader = { key = 'l', mods = 'CTRL', timeout_milliseconds = 1000 }
+    -- config.leader = { key = 'l', mods = 'CTRL', timeout_milliseconds = 1000 }
     config.keys = keys
     config.mouse_bindings = {
         -- Ctrl-click will open the link under the mouse cursor
