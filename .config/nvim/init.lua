1692cc1546c82d35c9ebb9fd3763558c18f101dc
Mathew Robinson <mrobinson@neurolabs.ai>
2025-07-29 08:30:45 +0100

    fix: add on_attach back using new config api

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -292,6 +292,10 @@ mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
 })
 
+vim.lsp.config('*', {
+  on_attach = on_attach,
+})
+
 -- mason_lspconfig.setup_handlers({
 --   function(server_name)
 --     require("lspconfig")[server_name].setup({
