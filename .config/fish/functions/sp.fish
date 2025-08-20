226075a856c09e558d1fe857e3d2a8f4255fcde5
Mathew Robinson <mrobinson@neurolabs.ai>
2025-08-20 09:17:49 +0100

    switch to compact bar for zellij

--- .config/fish/functions/sp.fish
+++ .config/fish/functions/sp.fish
@@ -5,8 +5,7 @@ function sp
         set -f project (projector find "(?i)$argv[1]")
     end
 
-    set -l exit_code $status
-    if test $exit_code != 0
+    if test $status != 0
         echo $project
         return $exit_code
     end
