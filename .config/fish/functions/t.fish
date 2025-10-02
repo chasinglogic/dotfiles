6515005740d1a29b031befa52b252550f27dac89
Mathew Robinson <mathew@chasinglogic.io>
2025-10-02 13:24:55 +0100

    theme tmux to my shell

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -1,14 +1,16 @@
 function t
     set -l session_name (basename (pwd))
-    if test -d ".git"
-        zellij --layout dev attach --create-background $session_name
-    else
-        zellij attach --create-background $session_name
+
+    if ! tmux has-session -t $session_name
+        if test -d .git
+            tmux new-session -d -s $session_name 'nvim .'
+            tmux new-window
+            tmux select-window -t 1
+        else
+            tmux new-session -d -s $session_name 
+        end
+
     end
 
-    if test -n "$ZELLIJ"
-        echo "Created session: $session_name"
-    else
-        zellij attach $session_name
-    end
+    tmux attach-session -t $session_name
 end
