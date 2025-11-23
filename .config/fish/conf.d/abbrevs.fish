2bd2e22aa690276bebefdca6b8f5f0a41cf77377
Mathew Robinson <mathew@chasinglogic.io>
2025-11-23 07:44:17 +0000

    daddy chill with the shell abbreviations

--- .config/fish/conf.d/abbrevs.fish
+++ .config/fish/conf.d/abbrevs.fish
@@ -1,40 +1,21 @@
-abbr --position anywhere --add ll "ls -alF"
-abbr --position anywhere --add la "ls -a"
-abbr --position anywhere --add l "ls -CF"
+abbr --add ll "ls -alF"
+abbr --add la "ls -a"
+abbr --add l "ls -CF"
 
-abbr --position anywhere --add g git
+abbr --add g git
 
 abbr --position anywhere --add k kubectl
 abbr --position anywhere --add kctx "kubectl config use-context"
 
-abbr --position anywhere --add tf tofu
-abbr --position anywhere --add tg terragrunt
-abbr --position anywhere --add grunt terragrunt
+abbr --add tf tofu
+abbr --add tg terragrunt
 abbr --add dotfiles "cd (dfm where)"
-
-abbr --position anywhere --add pm podman
-abbr --position anywhere --add dk docker
-abbr --position anywhere --add dce "docker compose exec -it"
-
-abbr --position anywhere --add zyp 'sudo zypper'
 
 abbr --add pu pulumi
 abbr --add pup "pulumi up"
 abbr --add sync-branches "git fetch origin --prune && git branch --merged | grep -v master | xargs git branch -D"
 
-abbr --add zyp "sudo zypper"
-abbr --add pac "sudo pacman"
-
-abbr --position anywhere --add aws-login-pip "aws --profile internal-services codeartifact login --tool pip --repository tillo-python --domain tillo --domain-owner 307488140247 --region eu-west-1"
-abbr --position anywhere --add aws-login-twine "aws --profile internal-services codeartifact login --tool twine --repository tillo-python --domain tillo --domain-owner 307488140247 --region eu-west-1"
-abbr --position anywhere --add aws-login-helm "aws --profile internal-services ecr get-login-password | helm registry login --username AWS --password-stdin 307488140247.dkr.ecr.eu-west-1.amazonaws.com"
-abbr --position anywhere --add aws-login-docker "AWS_PROFILE=internal-services aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 307488140247.dkr.ecr.eu-west-1.amazonaws.com"
-
-abbr --position anywhere --add vim $VIM_PROG
-
-abbr --position anywhere --add e $EDITOR
-
-abbr --position anywhere --add cd.. "cd .."
+abbr --add cd.. "cd .."
 
 abbr --add apt "sudo apt"
 abbr --add dnf "sudo dnf"
@@ -45,8 +26,6 @@ if test (uname) = Darwin
     abbr --position anywhere --add grep ggrep
 end
 
-abbr --position anywhere --add start-incident "wezterm record"
-
 function last_history_item
     echo $history[1]
 end
@@ -55,5 +34,5 @@ abbr --position anywhere --add !! --function last_history_item
 abbr --position anywhere --add gsutil "gcloud storage"
 
 if command -q bat
-	abbr --add cat bat
+	abbr --position anywhere --add cat bat
 end
