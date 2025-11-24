d486640849ad84d572e87a3d7dcbdf85a0d181b3
Mathew Robinson <mrobinson@neurolabs.ai>
2025-11-24 08:40:51 +0000

    add fancier lsp progress notifications

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -124,7 +124,36 @@ require('which-key').setup()
 require('dropbar').setup()
 -- }}}
 -- TreeSitter {{{
-require('nvim-treesitter').setup()
+require('nvim-treesitter').setup({
+    ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "dockerfile",
+        "embedded_template",
+        "go",
+        "gotmpl",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "kdl",
+        "lua",
+        "python",
+        "ruby",
+        "rust",
+        "svelte",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "zig",
+    }
+})
 -- }}}
 -- Mini {{{
 require('mini.pairs').setup()
@@ -304,6 +333,51 @@ nmap('<leader><leader>', '<CMD>Pick commands<CR>', '[P]ick vim commands')
 
 -- }}}
 -- LSP {{{
+
+---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local value = ev.data.params
+            .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        if not client or type(value) ~= "table" then
+            return
+        end
+        local p = progress[client.id]
+
+        for i = 1, #p + 1 do
+            if i == #p + 1 or p[i].token == ev.data.params.token then
+                p[i] = {
+                    token = ev.data.params.token,
+                    msg = ("[%3d%%] %s%s"):format(
+                        value.kind == "end" and 100 or value.percentage or 100,
+                        value.title or "",
+                        value.message and (" **%s**"):format(value.message) or ""
+                    ),
+                    done = value.kind == "end",
+                }
+                break
+            end
+        end
+
+        local msg = {} ---@type string[]
+        progress[client.id] = vim.tbl_filter(function(v)
+            return table.insert(msg, v.msg) or not v.done
+        end, p)
+
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+        vim.notify(table.concat(msg, "\n"), vim.log.levels.INFO, {
+            id = "lsp_progress",
+            title = client.name,
+            opts = function(notif)
+                notif.icon = #progress[client.id] == 0 and " "
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp-attach-config", { clear = true }),
