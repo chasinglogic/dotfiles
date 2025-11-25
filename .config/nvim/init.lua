dc4f1e809cfec40a2ff0c76ba63ccf7a57a74264
Mathew Robinson <mrobinson@neurolabs.ai>
2025-11-25 13:05:00 +0000

    setup erb-formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -299,6 +299,9 @@ require("mason-tool-installer").setup({
 -- }}}
 -- Autoformatting (conform) {{{
 require('conform').setup({
+    formatters_by_ft = {
+        eruby = { "erb_format" }
+    },
     format_on_save = {
         timeout_ms = 500,
         lsp_format = 'first',
