3642d9862d63d5292bf8cb22e1102d96345d8c46
Mathew Robinson <mrobinson@neurolabs.ai>
2025-11-26 05:27:31 +0000

    build st on top of existing functions for better functionality

--- .config/fish/functions/st.fish
+++ .config/fish/functions/st.fish
@@ -1,9 +1,4 @@
 function st
-    set -l session_name (tmux list-sessions | fzf | awk '{ print $1 }')
-
-    if test -n "$TMUX"
-        tmux switch-client -t $session_name
-    else
-        tmux attach-session -t $session_name
-    end
+    sp $argv
+    t
 end
