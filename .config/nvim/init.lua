0b0bd5dacfeb7661766c6ec39847657701879170
Mathew Robinson <chasinglogic@gmail.com>
2026-02-26 09:18:23 +0000

    add toggleterm to neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -173,7 +173,11 @@ MiniDeps.add({
 })
 -- Autoclose tags in TSX, HTML, etc.
 MiniDeps.add({
-    source = 'windwp/nvim-ts-autotag'
+    source = 'windwp/nvim-ts-autotag',
+})
+-- Toggle-able terminal
+MiniDeps.add({
+    source = 'akinsho/toggleterm.nvim',
 })
 -- }}}
 -- blink.cmp {{{
@@ -463,6 +467,36 @@ require('nvim-ts-autotag').setup({
         enable_close = true,         -- Auto close tags
         enable_rename = true,        -- Auto rename pairs of tags
         enable_close_on_slash = true -- Auto close on trailing </
+    },
+})
+-- }}}
+-- toggleterm.nvim {{{
+require("toggleterm").setup({
+    open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+    hide_numbers = true,      -- hide the number column in toggleterm buffers
+    autochdir = false,        -- when neovim changes it current directory the terminal will change it's own when next it's opened
+    start_in_insert = true,
+    insert_mappings = false,  -- whether or not the open mapping applies in insert mode
+    terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+    persist_size = true,
+    persist_mode = true,      -- if set to true (default) the previous terminal mode will be remembered
+    direction = 'float',
+    auto_scroll = true,       -- automatically scroll to the bottom on terminal output
+    -- This field is only relevant if direction is set to 'float'
+    float_opts = {
+        -- The border key is *almost* the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        border = 'single',
+        -- -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+        -- width = <value>,
+        -- height = <value>,
+        -- row = <value>,
+        -- col = <value>,
+        -- winblend = 3,
+        -- zindex = <value>,
+        title_pos = 'center',
     },
 })
 -- }}}
